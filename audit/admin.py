3f2e07e7b53c3c776af3bbd23eae78a9daf74365
SHIVAANSH0610_LUFFY <shivaansh0610@gmail.com>
2025-11-26 23:08:56 +0530

    added admin.py

--- audit/admin.py
+++ audit/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import WebPage, AuditRecord
 
-# Register your models here.
+
+@admin.register(WebPage)
+class WebPageAdmin(admin.ModelAdmin):
+    list_display = ("name", "url", "last_audit", "issues")
+    search_fields = ("name", "url")
+
+
+@admin.register(AuditRecord)
+class AuditRecordAdmin(admin.ModelAdmin):
+    list_display = ("page", "tool", "run_at", "issues_found")
+    list_filter = ("tool", "run_at")
+    search_fields = ("page__name", "page__url")
